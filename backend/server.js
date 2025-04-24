e146d88c2e7334a1caddf623787d9faef3fae12b
naziyaShaikh <naziyahm1234567@gmail.com>
2025-04-24 20:50:31 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const allowedOrigins = [
-  'https://authentication-jwt-0dlk.onrender.com'    // backend (if needed)
+  'https://authentication-jwt-1-8dxf.onrender.com', // <-- your frontend
+  'https://authentication-jwt-0dlk.onrender.com',    // backend (optional)
 ];
 
 app.use(cors({
